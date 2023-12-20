38a1f066ca35e688bbf2e8cca5188f2c9c63d4ed
Eduardo Hernand <byhernand@hotmail.com>
2023-12-19 19:05:52 -0600

    Calculator script

---
+++ calculator.js
@@ -0,0 +1,12 @@
+const add = (a, b) => console.log(a + b);
+const subtract = (a, b) => console.log(a - b);
+const multiply = (a, b) => console.log(a * b);
+const divide = (a, b) => console.log(a / b);
+
+
+// Testing
+
+add(320, 160);
+subtract(843, 70);
+multiply(50, 7);
+divide(100, 4);
